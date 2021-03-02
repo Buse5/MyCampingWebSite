c7acc17027cb0c294464cae14881ee4d7b0d5c94
BuseYalçın <buseylcin5@gmail.com>
2021-03-02 19:21:44 +0300

    first commit

---
+++ CampApı/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampApı.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<CampData.AuthorData> GetById(int entityID)
+        {
+            CampService.AuthorService authorServices = new CampService.AuthorService();
+            var data = authorServices.GetById(entityID);
+            if (data != null) return Ok(data);
+            return NotFound("Beklenmedik bir hata oluştu. Lütfen daha sonra tekrar deneyiniz!");
+        }
+
+      
+    }
+}
---
+++ CampApı/Startup.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampApı
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.EnableAnnotations();
+                c.SwaggerDoc("Web", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Camping Web API",
+                    Version = "1.0.0",
+                    Description = "Bu bir acıklamadır",
+                    TermsOfService = new System.Uri("http://swagger.io/terms/")
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            app.UseSwagger()
+               .UseSwaggerUI(c =>
+               {
+                   c.SwaggerEndpoint("/swagger/Web/swagger.json", "Web");
+               });
+        }
+    }
+}
